3fe8e076b163329f78d48458db5a7b1e98dce25a
Harshvardhan Singh <harshvardhansingh110306@gmail.com>
2026-08-06 19:32:48 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template,request
+import uuid
+import os
+
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'user_uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/create",methods=["GET","POST"])
+def create():
+    myid=uuid.uuid1()
+    if request.method == "POST":
+        print(request.files.keys())
+        req_id=request.form.get("uuid")
+        desc=request.form.get("text")
+        input_files=[]
+        for key,value in request.files.items():
+            print(key,value) 
+            file = request.files[key]
+            if file:
+                filename = secure_filename(file.filename)
+                if (not(os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'],req_id)))):
+                    os.mkdir(os.path.join(app.config['UPLOAD_FOLDER'],req_id))
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'],req_id, filename))
+                input_files.append(file.filename)
+                print(file.filename)
+
+            with open(os.path.join(app.config['UPLOAD_FOLDER'],req_id, "desc.txt"),"w") as f:
+                f.write(desc)
+            for fl in input_files:
+                with open(os.path.join(app.config['UPLOAD_FOLDER'],req_id, "input.txt"),"a") as f:
+                   f.write(f"file '{fl}'\n")
+                   f.write("duration 1\n")
+    return render_template("create.html",myid=myid)
+
+@app.route("/gallery")
+def gallery():
+    reels=os.listdir("static/reels")
+    print(reels)
+    return render_template("gallery.html",reels=reels)
+
+app.run(debug=True)
